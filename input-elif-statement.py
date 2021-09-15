741f4b59c6d6feceab53286d501ca01020e5a379
emkse <99069595@mydavinci.nl>
2021-09-15 09:42:12 +0200

    input-elif-statement

---
+++ input-elif-statement.py
@@ -0,0 +1,11 @@
+
+a = int(input("a = "))
+b = int(input("b = "))
+
+if a > b: 
+    Max = a
+    print("a is het grootste getal: ") + str(max)
+else:
+    Min = a 
+    print("a is het kleinste getal: ") + str(min)
+
